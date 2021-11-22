285e1cb64b475d0625abd7ded24ebcb620201b83
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2021-11-22 18:14:50 +0300

    Remove doc comments from 'actual' definitions

--- core/js/test/PlatformDefinitions.kt
+++ core/js/test/PlatformDefinitions.kt
@@ -10,46 +10,17 @@ import kotlinx.datetime.internal.JSJoda.Instant as jtInstant
 import kotlinx.datetime.internal.JSJoda.LocalDate as jtLocalDate
 import kotlinx.datetime.internal.JSJoda.LocalDateTime as jtLocalDateTime
 
-/**
- * The minimum [LocalDate] representable on the platform.
- */
-actual val LocalDate.Companion.MIN: LocalDate
-    get() = LocalDate(jtLocalDate.MIN)
+actual val LocalDate.Companion.MIN: LocalDate get() = LocalDate(jtLocalDate.MIN)
 
-/**
- * The maximum [LocalDate] representable on the platform.
- */
-actual val LocalDate.Companion.MAX: LocalDate
-    get() = LocalDate(jtLocalDate.MAX)
+actual val LocalDate.Companion.MAX: LocalDate get() = LocalDate(jtLocalDate.MAX)
 
-/**
- * The minimum [LocalDateTime] representable on the platform.
- */
-actual val LocalDateTime.Companion.MIN: LocalDateTime
-    get() = LocalDateTime(jtLocalDateTime.MIN)
+actual val LocalDateTime.Companion.MIN: LocalDateTime get() = LocalDateTime(jtLocalDateTime.MIN)
 
-/**
- * The maximum [LocalDateTime] representable on the platform.
- */
-actual val LocalDateTime.Companion.MAX: LocalDateTime
-    get() = LocalDateTime(jtLocalDateTime.MAX)
+actual val LocalDateTime.Companion.MAX: LocalDateTime get() = LocalDateTime(jtLocalDateTime.MAX)
 
-/**
- * The minimum [Instant] representable on the platform.
- */
-actual val Instant.Companion.MIN: Instant
-    get() = Instant(jtInstant.MIN)
+actual val Instant.Companion.MIN: Instant get() = Instant(jtInstant.MIN)
 
-/**
- * The maximum [Instant] representable on the platform.
- */
-actual val Instant.Companion.MAX: Instant
-    get() = Instant(jtInstant.MAX)
+actual val Instant.Companion.MAX: Instant get() = Instant(jtInstant.MAX)
 
-/**
- * Displays the given Instant in the given [offset].
- *
- * Be careful: this function may throw for some values of the [Instant].
- */
 actual fun Instant.toStringWithOffset(offset: UtcOffset): String =
     OffsetDateTime.ofInstant(this.value, offset.zoneOffset).toString()
--- core/jvm/test/PlatformDefinitionsJvm.kt
+++ core/jvm/test/PlatformDefinitionsJvm.kt
@@ -7,46 +7,17 @@ package kotlinx.datetime
 
 import java.time.*
 
-/**
- * The minimum [LocalDate] representable on the platform.
- */
-actual val LocalDate.Companion.MIN: LocalDate
-    get() = LocalDate(java.time.LocalDate.MIN)
+actual val LocalDate.Companion.MIN: LocalDate get() = LocalDate(java.time.LocalDate.MIN)
 
-/**
- * The maximum [LocalDate] representable on the platform.
- */
-actual val LocalDate.Companion.MAX: LocalDate
-    get() = LocalDate(java.time.LocalDate.MAX)
+actual val LocalDate.Companion.MAX: LocalDate get() = LocalDate(java.time.LocalDate.MAX)
 
-/**
- * The minimum [LocalDateTime] representable on the platform.
- */
-actual val LocalDateTime.Companion.MIN: LocalDateTime
-    get() = LocalDateTime(java.time.LocalDateTime.MIN)
+actual val LocalDateTime.Companion.MIN: LocalDateTime get() = LocalDateTime(java.time.LocalDateTime.MIN)
 
-/**
- * The maximum [LocalDateTime] representable on the platform.
- */
-actual val LocalDateTime.Companion.MAX: LocalDateTime
-    get() = LocalDateTime(java.time.LocalDateTime.MAX)
+actual val LocalDateTime.Companion.MAX: LocalDateTime get() = LocalDateTime(java.time.LocalDateTime.MAX)
 
-/**
- * The minimum [Instant] representable on the platform.
- */
-actual val Instant.Companion.MIN: Instant
-    get() = Instant(java.time.Instant.MIN)
+actual val Instant.Companion.MIN: Instant get() = Instant(java.time.Instant.MIN)
 
-/**
- * The maximum [Instant] representable on the platform.
- */
-actual val Instant.Companion.MAX: Instant
-    get() = Instant(java.time.Instant.MAX)
+actual val Instant.Companion.MAX: Instant get() = Instant(java.time.Instant.MAX)
 
-/**
- * Displays the given Instant in the given [offset].
- *
- * Be careful: this function may throw for some values of the [Instant].
- */
 actual fun Instant.toStringWithOffset(offset: UtcOffset): String =
     OffsetDateTime.ofInstant(this.value, offset.zoneOffset).toString()
